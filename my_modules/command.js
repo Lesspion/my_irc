7835c22b49deb1272eb862299dbbef021fbac6bb
lesspion <coquelet.c@gmail.com>
2016-01-27 09:27:01 +0100

    add condition to check if the script is called from the front or from the server

--- my_modules/command.js
+++ my_modules/command.js
@@ -4,4 +4,8 @@ var Command = {
     }
 };
 
-module.exports = Command;
+if (typeof module === "object" && module && typeof module.exports === "object") {
+    module.exports = Command;
+} else {
+    window.Command = Command;
+}
