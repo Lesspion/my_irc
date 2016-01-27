145a50b24fcc22949d296a4de8dc04b8ac798fb4
lesspion <coquelet.c@gmail.com>
2016-01-27 09:26:14 +0100

    add getter and setter for all attribut and add send function emit to server

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,15 +1,51 @@
-(function (global) {
+(function () {
     var user = {
         socket: null,
         nickname: null,
+        viewRoom: "",
+        allRoom: [],
         init: function (socket) {
             this.socket = socket;
         },
-        send: function (messageObject) {
-            this.socket.emit('message_is_coming', messageObject);
+        setNickname: function (name) {
+            this.nickname = name;
+        },
+        getNickname: function () {
+            return this.nickname;
+        },
+        setViewRoom: function (roomName) {
+            this.viewRoom = roomName;
+        },
+        getViewRoom: function () {
+            return this.viewRoom;
         },
         emptyInput: function () {
-            document.querySelector('my-msg').value = "";
+            document.querySelector('#my-msg').value = "";
+        },
+        addRoom: function (roomName) {
+            this.allRoom.push(roomName);
+        },
+        getAllRoom: function () {
+            return this.allRoom;
+        },
+        removeRoom: function (roomName) {
+            var index = this.allRoom.indexOf(roomName);
+            if (index !== -1) {
+                this.allRoom.remove(index);
+            }
+            if (roomName === this.getViewRoom()) {
+                this.setViewRoom(null);
+            }
+        },
+        send: function () {
+            var obj = {};
+            obj.nickname = this.nickname;
+            obj.room = this.getViewRoom();
+            obj.message = document.querySelector('#my-msg').value;
+            this.socket.emit('message_is_coming', obj);
+            this.emptyInput();
         }
     };
+    
+    window.user = user;
 })(this);
