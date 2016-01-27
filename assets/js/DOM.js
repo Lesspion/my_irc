a8614eb6713e10866bce27eda63899d406e69894
lesspion <coquelet.c@gmail.com>
2016-01-27 09:55:14 +0100

    added addChannel prototype method

--- assets/js/DOM.js
+++ assets/js/DOM.js
@@ -1,6 +1,16 @@
 (function () {
     DOM = {};
+    
     DOM.prototype.addMessage = function(DOMMessage) {
         document.querySelector('.scrollable').appendChild(DOMMessage); // TO-TEST
     };
+    
+    DOM.prototype.addChannel = function (DOMChannel) {
+        document.querySelector('.fixed').appendChild(DOMChannel);
+    };
+    
+    DOM.prototype.removeChannel = function () {
+        
+    };
+    
 }).call(this);
