2ecbec0ac2210234ce00b0f8d1d3555e8e0fa92d
lesspion <coquelet.c@gmail.com>
2016-01-27 09:26:37 +0100

    change setNickname and add user.send to send message

--- assets/js/mainSocket.js
+++ assets/js/mainSocket.js
@@ -1,21 +1,25 @@
+// socket party !!!!!
 var socket = io.connect('http://localhost:1664');
 
 socket.on('my_name_is', function (name) {
-    user.nickname(name);
+    user.setNickname(name);
 });
 
+
+// DOM party !!!!!!!!
 document.addEventListener('DOMContentLoaded', function () {
     var send = document.querySelector('.my-send');
     var input = document.querySelector('#my-msg');
     
     send.addEventListener('click', function () {
-        
+        user.send();
     });
     
     input.addEventListener('keydown', function (e) {
         var key = e.keyCode || e.which;
         if (key === 13) {
             // enter ok;
+            user.send()
         }
     });
 });
