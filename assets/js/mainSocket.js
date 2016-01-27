e57b1db7dd652d6893e86a29a86e48679f36d81b
lesspion <coquelet.c@gmail.com>
2016-01-27 19:10:55 +0100

    add global for visual studio code && test setViewRoom room1 && move socket code to event dom content loaded

--- assets/js/mainSocket.js
+++ assets/js/mainSocket.js
@@ -1,23 +1,41 @@
-// socket party !!!!!
-var socket = io.connect('http://localhost:1664');
-
-socket.on('my_name_is', function (name) {
-    user.setNickname(name);
-});
-
-socket.on('listen_me', function (msgObject) {
-    // condition if it's me or not
-});
+/* global io */
+/* global DOMHistory */
+/* global user */
+/* global DOMElement */
+/* global DOM */
 
 
 // DOM party !!!!!!!!
 document.addEventListener('DOMContentLoaded', function () {
-    user.init(socket);
+    
     var send = document.querySelector('.my-send');
     var input = document.querySelector('#my-msg');
     
-    send.addEventListener('click', function () {
+    // socket party !!!!!
+    var socket = io.connect('http://localhost:1664');
+    
+    user.init(socket);
+    
+    socket.on('my_name_is', function (name) {
+        user.setNickname(name);
+        user.setViewRoom('room1');
+
+    });
+
+    socket.on('listen_me', function (msgObject) {
+        // condition if it's me or not
+        var me = msgObject.user === user.nickname ? true : false;
+        console.log('I have : ', msgObject);
+        if (user.getViewRoom() === msgObject.room) {
+            DOM.addMessage(DOMElement.newMessage(msgObject.message, msgObject.nickname, me));
+        } else {
+            // DOMHistory.allMessages[msgObject.room] += DOMElement.newMessage(msgObject.message, msgObject.user, me);
+        }
+    });
+    
+    send.addEventListener('click', function (e) {
         user.send();
+        e.preventDefault();
     });
     
     input.addEventListener('keydown', function (e) {
@@ -25,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (key === 13) {
             // enter ok;
             user.send();
+            e.preventDefault();
         }
     });
 });
