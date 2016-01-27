ade84e3fb8f3dbd8b47e2831aaaea7a8395029ef
lesspion <coquelet.c@gmail.com>
2016-01-27 19:10:06 +0100

    push domelement to window && remove prototype && modify color name

--- assets/js/DOMElement.js
+++ assets/js/DOMElement.js
@@ -2,11 +2,12 @@
     
     var DOMElement = {};
     
-    DOMElement.prototype.newMessage = function (messageContent, nickname, me) {
+    DOMElement.newMessage = function (messageContent, nickname, me) {
         var s = !me ? 's6' : 'offset-s6';
         var isItMe = !me ? 'not-me' : 'me'
+        var color = !me ? 'blue-grey' : 'grey';
         return '<div class="col ' + s + '" style="height: 200px;">' + 
-                   '<div class="card blue-grey darken-1">' +
+                   '<div class="card ' + color + ' darken-1">' +
                        '<div class="card-content white-text">' +
                            '<span class="card-title ' + isItMe + '">' + nickname + '</span>' +
                                 '<p>' + messageContent + '</p>' +
@@ -15,8 +16,10 @@
                '</div>';
     };
     
-    DOMElement.prototype.newChannel = function (channelName) {
+    DOMElement.newChannel = function (channelName) {
         return '<div class="card-panel hoverable center-align" data-channelName="' + channelName + '" onclick="user.switchChannel(' + channelName + ')">' + channelName + '</div>';
     };
+    
+    window.DOMElement = DOMElement;
     
 }).call(this);
