2d2656499b470c9fde2aed71e3f7f23cf40d2aa9
lesspion <coquelet.c@gmail.com>
2016-01-27 09:48:25 +0100

    Create DOMELement class to simplify the creation of tag for message and channel

---
+++ assets/js/DOMElement.js
@@ -0,0 +1,18 @@
+(function () {
+    var DOMElement = {};
+    DOMElement.prototype.newMessage = function (messageContent, nickname, me) {
+        var s = !me ? 's6' : 'offset-s6';
+        var isItMe = !me ? 'not-me' : 'me'
+        return '<div class="col ' + s + '" style="height: 200px;">' + 
+                   '<div class="card blue-grey darken-1">' +
+                       '<div class="card-content white-text">' +
+                           '<span class="card-title ' + isItMe + '">' + nickname + '</span>' +
+                                '<p>' + messageContent + '</p>' +
+                       '</div>' +
+                   '</div>' +
+               '</div>';
+    };
+    DOMElement.prototype.newChannel = function (channelName) {
+        return '<div class="card-panel hoverable center-align">' + channelName + '</div>';
+    };
+}).call(this);
