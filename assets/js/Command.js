3443f9c3b93a98aebd2eadfeb32b0f687ba50b37
lesspion <coquelet.c@gmail.com>
2016-01-29 00:56:24 +0100

    Command js

--- assets/js/Command.js
+++ assets/js/Command.js
@@ -1,54 +1,69 @@
 (function () {
    
    var Command = {
-       socket: null,
-       reg: null,
-       matches: null,
-   };
-   
-   Command.init = function (socket) {
-       this.socket = socket;
-   };
-   
-   Command.analyze = function (str) {
-       console.log(this.matches)
-       for (var action in Command.list()) {
-           if (this.matches[0] && this.matches[0] === action) {
-               Command.list()[this.matched[0]]();
+       matches: [],
+       init: function (user) {
+           this.user  = user;
+       },
+       analyze: function () {
+           console.log(Command.matches);
+           var that = this;
+           for (var action in Command.list()) {
+               if (that.matches[0] && that.matches[0] === action) {
+                   Command.list()[that.matches[0]]();
+               }
            }
-       }
-   };
-   
-   Command.search = function (str) {
-       this.reg = new RegExp('#^/(.*)\s(.*)$');
-       this.matches = this.reg.exec(str);
-       this.analyze(str);
+        },
+        search: function (str) {
+           if (str[0] === '/') {
+               Command.matches = str.split('/').join('').split(' ');
+               this.analyze();
+           } else {
+               user.send();
+           }
+        },
+        nickAction: function () {
+            socket.emit("say_my_name", Command.matches[1]);
+            user.setNickname(Command.matches[1]);
+        }
    };
    
    Command.joinAction = function () {
-       this.socket.emit('join_the_dark_side', this.matches[1]);
+       socket.emit('join_the_dark_side', Command.matches[1]);
        // (switch de vue);
+       DOMHistory.setMessages(user.getViewRoom(), document.querySelector('.scrollable'));
+       user.setViewRoom(Command.matches[1]);
+       user.allRoom.push(Command.matches[1]);
+       if (DOMHistory.allMessages[Command.matches[1]]) {
+           console.log('JE suis la : ');
+           console.log(DOMHistory.allMessages[Command.matches[1]]);
+           var scrollable = document.querySelector('.scrollable');
+           scrollable.parentNode.replaceChild(scrollable, DOMHistory.getMessages(Command.matches[1]));
+       }
    };
    
    Command.listAction = function () {
-       this.socket.emit('it_s_just_over_9000');
+       socket.emit('it_s_just_over_9000');
    };
    
    Command.meAction = function (action) {
        // maybe after
    };
    
-   Command.nickAction = function () {
-       this.socket.emit('say_my_name', this.matches[1]);
-       // save in local user.setNickname(newNick);
-   };
-   
    Command.leaveAction = function () {
-       this.socket.emit('justin_leave_r', this.matches[1]);
+       socket.emit('justin_leave_r', Command.matches[1]);
    };
    
-   Command.privateMessageAction = function () {
+   Command.userAction = function () {
+       
+   };
+   
+   Command.messageAction = function () {
        // TO-THINK
+   };
+   
+   Command.helpAction = function () {
+       socket.emit('help_me');
    };
    
    Command.list = function () {
@@ -59,10 +74,12 @@
            'me'    : that.meAction,
            'nick'  : that.nickAction,
            'part'  : that.leaveAction,
-           'users' : that.privateMessageAction
+           'user' : that.userAction,
+           'msg'   : that.messageAction,
+           'help'  : that.helpAction
        };
    };
     
     window.Command = Command;
     
-});
+}).call(this);
