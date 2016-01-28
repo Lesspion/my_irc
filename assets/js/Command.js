bed3ef41802cbef45e92ac4ad8b7d872c7cbfbba
lesspion <coquelet.c@gmail.com>
2016-01-28 15:13:53 +0100

    add all action command and init command

--- assets/js/Command.js
+++ assets/js/Command.js
@@ -1,15 +1,20 @@
 (function () {
    
    var Command = {
+       socket: null,
        reg: null,
        matches: null,
+   };
+   
+   Command.init = function (socket) {
+       this.socket = socket;
    };
    
    Command.analyze = function (str) {
        console.log(this.matches)
        for (var action in Command.list()) {
            if (this.matches[0] && this.matches[0] === action) {
-               Command.list()[this.matched[0]](this.matched[1]);
+               Command.list()[this.matched[0]]();
            }
        }
    };
@@ -17,28 +22,29 @@
    Command.search = function (str) {
        this.reg = new RegExp('#^/(.*)\s(.*)$');
        this.matches = this.reg.exec(str);
+       this.analyze(str);
    };
    
-   Command.joinAction = function (channelName) {
-       socket.emit('join_the_dark_side', channelName);
+   Command.joinAction = function () {
+       this.socket.emit('join_the_dark_side', this.matches[1]);
        // (switch de vue);
    };
    
    Command.listAction = function () {
-       socket.emit('it_s_just_over_9000');
+       this.socket.emit('it_s_just_over_9000');
    };
    
    Command.meAction = function (action) {
        // maybe after
    };
    
-   Command.nickAction = function (newNick) {
-       socket.emit('say_my_name', newNick);
+   Command.nickAction = function () {
+       this.socket.emit('say_my_name', this.matches[1]);
        // save in local user.setNickname(newNick);
    };
    
-   Command.leaveAction = function (channelName) {
-       socket.emit('justin_leave_r', channelName);
+   Command.leaveAction = function () {
+       this.socket.emit('justin_leave_r', this.matches[1]);
    };
    
    Command.privateMessageAction = function () {
